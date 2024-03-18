ef00b4f01412d032f1c6cb09bb89a2b102aa5ca4
Andresa Vieira <andresacg_14@hotmail.com>
2024-03-18 20:44:04 -0300

    melhoria na estilização

--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
 const content = document.querySelector(".content");
 const inputSearch = document.querySelector("input[type='search']");
+const btnLimpar = document.querySelector("#refresh");
 
 let items = [];
 
 inputSearch.oninput = () => {
   content.innerHTML = "";
-
   items
+
     .filter((item) =>
       item.toLowerCase().includes(inputSearch.value.toLowerCase())
     )
-    .forEach((item) => addHTML(item));
+    .forEach((item) => printInfoApi(item));
 };
 
-function addHTML(item) {
+function printInfoApi(item) {
   const div = document.createElement("div");
   div.innerHTML = item;
   content.append(div);
@@ -22,22 +23,26 @@ function addHTML(item) {
 fetch("https://jsonplaceholder.typicode.com/users")
   .then((data) => data.json())
   .then((users) => {
-    users.forEach((user) => {
-        
-      addHTML(user.name);
-      addHTML(user.username);
-      addHTML(user.email);
-      addHTML(user.phone);
-      addHTML(user.website);
-      addHTML(user.company.name);
-      addHTML(user.company.catchPhrase);
 
-      items.push(user.name);
-      items.push(user.username);
-      items.push(user.email);
-      items.push(user.phone);
-      items.push(user.website);
-      console.log(items);
+    users.forEach((user) => {
+        printInfoApi(user.name);
+        printInfoApi(user.username);
+        printInfoApi(user.email);
+        printInfoApi(user.phone);
+        printInfoApi(user.website);
+        printInfoApi(user.company.name);
+        printInfoApi(user.company.catchPhrase);
+
+        items.push(user.name);
+        items.push(user.username);
+        items.push(user.email);
+        items.push(user.phone);
+        items.push(user.website);
+
     });
     
+});
+
+btnLimpar.addEventListener('click', () => {
+    window.location.reload();
 });
