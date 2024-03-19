3ccc8841d476c24e4c9792678f1d0aee0e7fd047
Andresa Vieira <andresacg_14@hotmail.com>
2024-03-18 22:59:10 -0300

    add propriedades

--- src/main.js
+++ src/main.js
@@ -4,39 +4,51 @@ const btnLimpar = document.querySelector("#refresh");
 
 let items = [];
 
+//campo de busca em todas as propriedades da array
 inputSearch.oninput = () => {
   content.innerHTML = "";
   items
-
-    .filter((item) =>
-      item.toLowerCase().includes(inputSearch.value.toLowerCase())
+    .filter((user) =>
+      Object.values(user).some(
+        (value) =>
+          typeof value === "string" &&
+          value.toLowerCase().includes(inputSearch.value.toLowerCase())
+      )
     )
-    .forEach((item) => printInfoApi(item));
+    .forEach((user) => printInfoApi(user));
 };
 
-function printInfoApi(item) {
-  const div = document.createElement("div");
-  div.innerHTML = item;
-  content.append(div);
+//printa na tela
+function printInfoApi(user) {
+  const userDiv = document.createElement("div");
+  userDiv.classList.add("user");
+  userDiv.innerHTML = `
+      <div class="card">
+        <br><strong>Name:</strong>${user.name}
+        <br><strong>Username:</strong> ${user.username}
+        <br><strong>Email:</strong> ${user.email}
+        <br><strong>Contato:</strong> ${user.phone}
+        <br><strong>Website:</strong> ${user.website}
+        <br><strong>Razão social:</strong> ${user.company.name}
+        <br><strong>Slogan da empresa:</strong> ${user.company.catchPhrase}
+        <br><strong>Valores:</strong> ${user.company.bs}
+        <br><strong>Street:</strong> ${user.address.street},${user.address.suite}, ${user.address.city}
+        <br><strong>CEP:</strong> ${user.address.zipcode}
+        <br><strong>Geolocalização:</strong> Latitude = ${user.address.geo.lat} Longitude = ${user.address.geo.lng} 
+        
+        <br>
+      </div>
+  `;
+  content.append(userDiv);
 }
 
+//Fetch na API
 fetch("https://jsonplaceholder.typicode.com/users")
   .then((data) => data.json())
   .then((users) => {
     users.forEach((user) => {
-      printInfoApi(user.name);
-      printInfoApi(user.username);
-      printInfoApi(user.email);
-      printInfoApi(user.phone);
-      printInfoApi(user.website);
-      printInfoApi(user.company.name);
-      printInfoApi(user.company.catchPhrase);
-
-      items.push(user.name);
-      items.push(user.username);
-      items.push(user.email);
-      items.push(user.phone);
-      items.push(user.website);
+      items.push(user);  // Adiciona o objeto de usuário completo à lista items
+      printInfoApi(user); // Passa o objeto user completo
     });
   });
 
